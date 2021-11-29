d702642e9cff96dd19e86321bc054667118310e5
Jan Mewes <jan.mewes@epages.com>
2021-11-29 18:46:17 +0100

    Add markdown editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Editor from "react-markdown-editor-lite";
+import ReactMarkdown from "react-markdown";
+import "react-markdown-editor-lite/lib/index.css";
+
+import "./App.css"
 
 function App() {
+  const mdEditor = React.useRef(null);
+  const [value, setValue] = React.useState("xxx");
+
+  const handleClick = () => {
+    if (mdEditor.current) {
+      alert(mdEditor.current.getMdValue());
+    }
+  };
+
+  const handleEditorChange = ({ html, text }) => {
+    const newValue = text.replace(/\d/g, "");
+    console.log(newValue);
+    setValue(newValue);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>Get value</button>
+      <Editor
+        ref={mdEditor}
+        value={value}
+        style={{
+          height: "500px"
+        }}
+        onChange={handleEditorChange}
+        renderHTML={text => <ReactMarkdown source={text} />}
+      />
     </div>
   );
 }
