2b0a2353604a4803909fbfb74b252403ca982dad
Jan Mewes <jan.mewes@epages.com>
2021-11-30 18:39:12 +0100

    Improve layout of editor page

--- src/views/TicketEditor.jsx
+++ src/views/TicketEditor.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Editor from "react-markdown-editor-lite";
 import ReactMarkdown from "react-markdown";
-import "react-markdown-editor-lite/lib/index.css";
 
-import "../App.css"
+import './TicketEditor.css';
+import "react-markdown-editor-lite/lib/index.css";
+import Footer from '../components/Footer';
+
+
 
 function TicketEditor() {
   const mdEditor = React.useRef(null);
-  const [value, setValue] = React.useState();
+  const [value, setValue] = React.useState("Hello");
 
   const handleClick = () => {
     if (mdEditor.current) {
@@ -62,19 +65,37 @@ function TicketEditor() {
 
   return (
     <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        <div className="container px-5">
+          <h1>Story template</h1>
+        </div>
+      </nav>
       
-      <button onClick={handleClick}>Copy to clipboard</button>
-      <Editor
-        ref={mdEditor}
-        value={value}
-        style={{
-          height: "500px"
-        }}
-        view={{html: false}}
-        onChange={handleEditorChange}
-        renderHTML={text => <ReactMarkdown source={text} />}
-      />
+      <header className="masthead">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+    
+            <div className="editor">
+              <Editor
+                ref={mdEditor}
+                value={value}
+                style={{
+                  height: "500px"
+                }}
+                view={{html: false}}
+                onChange={handleEditorChange}
+                renderHTML={text => <ReactMarkdown source={text} />}
+              />
+            </div>
+
+            
+          </div>
+        </div>
+      </header>
       
+      <div className="fixedFooter">
+        <Footer />
+      </div>
     </div>
   );
 }
