35261eb0760d24ff4b065652cf882ecbe04ff3d3
Jan Mewes <jan.mewes@epages.com>
2021-11-30 18:12:18 +0100

    Make page title nice

--- src/views/Start.jsx
+++ src/views/Start.jsx
@@ -8,18 +8,25 @@ import { Link } from 'react-router-dom';
 function Start() {
   return (
     <div className="App">
-      <div className="container px-5">
-        <div className="row gx-5 align-items-center">
-          <div className="col-lg-12">
-            <h1>Ticket templates</h1>
-            <ul>
-              <li><Link to="/story">Story</Link></li>
-              <li><Link to="/api">API</Link></li>
-              <li><Link to="/bug">Bug</Link></li>
-            </ul>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        <div className="container px-5">
+          <h1>Ticket templates</h1>
+        </div>
+      </nav>
+      
+      <header className="masthead">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center">
+            <div className="col-lg-12">
+              <ul>
+                <li><Link to="/story">Story</Link></li>
+                <li><Link to="/api">API</Link></li>
+                <li><Link to="/bug">Bug</Link></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      </header>
       
       <div className="fixedFooter">
         <Footer />
